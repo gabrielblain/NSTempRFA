4013eb0a0403aad07abf3b8873f85987282779ad
gabrielblain <gabrielblain@gmail.com>
2025-08-18 14:42:46 -0300

    Add new data file

---
+++ R/TmaxCPC_SP.R
@@ -0,0 +1,16 @@
+#' Extreme air temperature data
+#'
+#' Daily extremes for air temperature data sampled from
+#' the block maxima approach. A block corresponds to one year.
+#' The data belongs to The Climate Prediction Center - NOAA.
+#' location of Ribeirao Preto, State of Sao Paulo in Brazil.
+#'
+#' @format ## `TmaxCPC_SP`
+#' A data.frame with 14 columns and 34 rows.
+#'
+#' \describe{
+#' \item{Year}{Year}
+#' \item{Pixel_1 to Pixel_13}{daily extremes for air temperature}
+#' }
+#' @source <https://psl.noaa.gov/>
+"TmaxCPC_SP"
---
+++ R/lonlat_Tmax.R
@@ -0,0 +1,12 @@
+#' Geographical coordinates of the `TmaxCPC_SP` series
+#'
+#'
+#' @format ## `lonlat_Tmax`
+#' A data.frame with 2 columns and 13 rows.
+#'
+#' \describe{
+#' \item{lon}{Longitude in decinal degrees}
+#' \item{lat}{Latitude in decinal degrees}
+#' }
+#' @source <https://psl.noaa.gov/>
+"lonlat_Tmax"
